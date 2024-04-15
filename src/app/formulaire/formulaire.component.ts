5ededfe74e0430034aca99ba31670ecede638beb
oussema.sellami <oussema.sellami@esprit.tn>
2024-04-15 16:43:57 +0100

    formulaire done

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -6,17 +6,25 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './formulaire.component.html',
   styleUrls: ['./formulaire.component.css']
 })
+
+/*export function forbiddenNameValidator(x:string){
+  if(x==="arctic"){
+return true
+  }
+  return false
+}*/
 export class FormulaireComponent implements OnInit {
   formresidence!:FormGroup
   var1:string="bonjour"
 ngOnInit(): void {
-    
+  
   this.formresidence= new FormGroup({
-    id:new FormControl("",Validators.required),
-    name:new FormControl("",Validators.required),
-    address:new FormControl("",Validators.required)
+    id:new FormControl("",[Validators.required,Validators.pattern('^[1-9]*$')]),
+    name:new FormControl("",[Validators.required,Validators.minLength(4)]),
+    address:new FormControl("",[Validators.required,Validators.pattern('^[A-Za-z]*$')/*forbiddenNameValidator(/arctic/i)*/])
   })
 }
+get name(){return this.formresidence.get("name")}
 
 add(){
   console.log(this.formresidence.value)
